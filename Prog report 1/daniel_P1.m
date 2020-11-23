a5465d96fdb069d495dde049abdb993b8668d348
Daniel Crook <danielcrookca@gmail.com>
2020-11-23 12:30:20 -0800

    daniel part 1 code

---
+++ Prog report 1/daniel_P1.m
@@ -0,0 +1,120 @@
+A = @(vg,thetag,va,thetaa) [0 0 -vg*sin(thetag) 0 0 0;...
+    0 0 vg*cos(thetag) 0 0 0;...
+    0 0 0 0 0 0;...
+    0 0 0 0 0 -va*sin(thetaa);...
+    0 0 0 0 0 va*cos(thetaa);...
+    0 0 0 0 0 0];
+
+B = @(thetag,L,phig,vg,thetaa) [cos(thetag) 0 0 0;...
+    sin(thetag) 0 0 0;...
+    tan(phig)/L (vg/L)*sec(phig)^2 0 0;...
+    0 0 cos(thetaa) 0;...
+    0 0 sin(thetaa) 0;...
+    0 0 0 1];
+
+C = @(xig,etag,xia,etaa) [((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2)...
+    -((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2) -1 ...
+    -((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2)...
+    ((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2) 0;...
+    (xig-xia)/sqrt((xig-xia)^2+(etag-etaa)^2)...
+    (etag-etaa)/sqrt((xig-xia)^2+(etag-etaa)^2) 0 ...
+    -(xig-xia)/sqrt((xig-xia)^2+(etag-etaa)^2)...
+    (etag-etaa)/sqrt((xig-xia)^2+(etag-etaa)^2) 0;...
+    ((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2)...
+    -((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2) 0 ...
+    -((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2)...
+    ((etaa-etag)/(xia-xig)^2)/(1+((etaa-etag)/(xia-xig))^2) -1;...
+    0 0 0 1 0 0; 0 0 0 0 1 0];
+
+D = zeros(6,4);
+
+L = 0.5;
+xig = 10;
+etag = 0;
+thetag = pi/2;
+vg = 2;
+phig = -pi/18;
+xia = -60;
+etaa = 0;
+thetaa = -pi/2;
+va = 12;
+wa = pi/25;
+
+Alin = A(vg,thetag,va,thetaa);
+Blin = B(thetag,L,phig,vg,thetaa);
+Clin = C(xig,etag,xia,etaa);
+Dlin = D;
+
+DT = 0.1;
+
+z = expm(DT*[Alin Blin; zeros(4,10)]);
+F = z(1:6,1:6);
+G = z(1:6,7:10);
+H = Clin;
+M = Dlin;
+
+x0 = [xig etag thetag xia etaa thetaa]';
+x = x0;
+u = [vg phig va wa]';
+
+% for k=1:1000
+%     x(:,k+1) = F*x(:,k) + G*u;
+%     y(:,k) = H*x(:,k+1);
+% end
+
+
+%% Full Nonlinear Perturbation Dynamics
+% tspan = [0,100];
+tspan = 0:0.01:100;
+x0 = x0 + [0 1 0 0 0 0.1]';
+[t,y] = ode45(@(t,y) NLcoop(t,y,u,L),tspan,x0);
+
+% correct rad data
+for i=1:length(t)
+    thetag = y(i,3);
+    thetaa = y(i,6);
+    if thetag>2*pi
+        y(i,3)=thetag-2*pi;
+    elseif thetag<-2*pi
+        y(i:end,3)=y(i:end,3)+2*pi;
+    end
+    
+    if thetaa>2*pi
+        y(i:end,6)=y(i:end,6)-2*pi;
+    elseif thetaa<-2*pi
+        y(i,6)=thetaa+2*pi;
+    end
+end
+
+% States vs Time, Full NL Dynamics Simulation
+% tk = linspace(0,k/10,k+1);
+figure
+subplot(6,1,1); hold on; grid on; grid minor
+% plot(tk,x(1,:))
+plot(t,y(:,1))
+hold off
+
+subplot(6,1,2); hold on; grid on; grid minor
+% plot(tk,x(2,:))
+plot(t,y(:,2))
+hold off
+
+subplot(6,1,3); hold on; grid on; grid minor
+% plot(tk,x(3,:))
+plot(t,y(:,3))
+hold off
+
+subplot(6,1,4); hold on; grid on; grid minor
+% plot(tk,x(4,:))
+plot(t,y(:,4))
+hold off
+
+subplot(6,1,5); hold on; grid on; grid minor
+% plot(tk,x(5,:))
+plot(t,y(:,5))
+hold off
+
+subplot(6,1,6); hold on; grid on; grid minor
+% plot(tk,x(6,:))
+plot(t,y(:,6))
+hold off
