85ae18cf2d9261495efbcb23d616e71ee480a26b
Daniel Crook <danielcrookca@gmail.com>
2020-11-24 15:33:20 -0800

    ode function

---
+++ Prog report 1/NLcoop.m
@@ -0,0 +1,45 @@
+function dx = NLcoop(~,y,u,g)
+%NLCOOP Summary of this function goes here
+%   Detailed explanation goes here
+
+
+thetag = y(3);
+thetaa = y(6);
+
+vg = u(1);
+phig = u(2);
+va = u(3);
+wa = u(4);
+
+% switch cases
+if wa > pi/6
+    wa = pi/6;
+elseif wa < -pi/6
+    wa = -pi/6;
+end
+
+if va > 20
+    va = 20;
+elseif va < 10
+    va = 10;
+end
+
+if phig > 5*pi/12
+    phig = 5*pi/12;
+elseif phig < -5*pi/12
+    phig = -5*pi/12;
+end
+
+if vg > 3
+    vg = 3;
+end
+
+
+
+L = g;
+
+dx = [vg*cos(thetag); vg*sin(thetag); vg*tan(phig)/L;...
+    va*cos(thetaa); va*sin(thetaa); wa];
+
+end
+
